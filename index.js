caea6cdc8a67855f2371cf4db6745761e96ec8fd
Kimberly Huynh <huynh.h.kimberly@gmail.com>
2019-09-12 04:26:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //takeANumber adds customer to line then returns its name and place in line
 var katzDeli = [];
 
@@ -31,34 +30,3 @@ function currentLine(line) {
   return(`The line is currently:` + array);
   }
 }
-=======
-var katzDeli = [];
-
-function takeANumber(katzDeli, name) {
-  katzDeli.push(name);
-  return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
-}
-
-function nowServing(katzDeli) {
- if (katzDeli.length > 0) {
-   var nowServing = katzDeli[0];
-   katzDeli.shift();
-    return(`Currently serving ${nowServing}.`);
-  } else { 
-  return (`There is nobody waiting to be served!`);
-  }
-}
-
-function currentLine(line) {
-  if (line.length === 0) {
-    return (`The line is currently empty.`);
-  } else {
-    
-    var array = [];
-    for (var i = 0; i < line.length; i++) {
-      array.push(` ${i+1}. ${line[i]}` );
-    }
-  return(`The line is currently:` + array);
-  }
-}
->>>>>>> 7ae545cc8e3a984458f4fe4e0b7cefe5938884c4
